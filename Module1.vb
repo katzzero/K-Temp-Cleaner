ed818e10c3cfd6b37ff19af5597a43f1389939cb
katzzero <katzzero@gmail.com>
2014-06-12 14:11:23 -0300

    First bytes of code...
    Just the basics ...

---
+++ Module1.vb
@@ -0,0 +1,32 @@
+﻿Module Module1
+
+    Sub Main()
+        Console.WriteLine("..== For testing purpose only, made by T.O.Neves and Sergio Pastore ==..")
+        Dim tempath As String = System.IO.Path.GetTempPath
+        Console.Read()
+        Console.WriteLine(tempath)
+        Dim filelist = System.IO.Directory.EnumerateFiles(tempath)
+        Try
+            For Each temps In filelist
+                Console.WriteLine(temps)
+            Next
+            Console.Read()
+        Catch ex As Exception
+            Console.WriteLine("error acessing temp files")
+        End Try
+        Try
+            For Each temps In filelist
+                Try
+                    System.IO.File.Delete(temps)
+                Catch ex As Exception
+                    Console.WriteLine("error deleting file" & 
+                End Try
+                Console.Read()
+            Next
+        Catch ex As Exception
+            Console.WriteLine("error acessing temp files")
+        End Try
+
+    End Sub
+
+End Module
